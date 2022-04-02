c461222d640336754e8eca09f5e1b009a04396ab
Forest Anderson <forestkzanderson@gmail.com>
2022-04-01 23:08:49 -0400

    Laptop timing

--- Main.gd
+++ Main.gd
@@ -5,7 +5,20 @@ extends Control
 # var a = 2
 # var b = "text"
 
+# Design
+# 
+# Starting
+# - Buy laptop
+# - Get first few currencies
+# 
+# Mid
+# 
+
 var money: float = 0.0
+
+var levels = [
+	0, # Laptop updates
+]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,8 +28,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	money += delta
+	var gain = 0
+	
+	var laptop_per_second_per_level = 0.5
+	gain += 0.5 * delta * levels[0]
+	
+	money += gain
 	
 	var amount = int(money)
 	
-	$HBoxContainer/Money.text = str(amount)
+	
+	$HBoxContainer/LeftPanel/Money.text = str(amount)
+
+
+func _on_Button_pressed():
+	levels[0] += 1
